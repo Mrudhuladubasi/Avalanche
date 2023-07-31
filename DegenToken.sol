d800924b671282052f053e9cf10850279ee8ed88
Mrudhula Dubasi <137142944+Mrudhuladubasi@users.noreply.github.com>
2023-08-01 00:21:41 +0530

    DegenToken.sol
    Minting new tokens: The platform should be able to create new tokens and distribute them to players as rewards. Only the owner can mint tokens.
    Transferring tokens: Players should be able to transfer their tokens to others.
    Redeeming tokens: Players should be able to redeem their tokens for items in the in-game store.
    Checking token balance: Players should be able to check their token balance at any time.
    Burning tokens: Anyone should be able to burn tokens, that they own, that are no longer needed

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,36 +1,59 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable 
 {
+
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol)
     {
         _mint(msg.sender, initialSupply * (10 ** decimals()));
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function mint(address to, uint256 mint_amt) public onlyOwner {
+        _mint(to, mint_amt);
     }
 
-    function redeem(uint256 amount) public {
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-        _burn(msg.sender, amount);
+  
+    function game_items() public pure returns (string memory)
+    {
+        return ("1. Ball gown = 50 \n 2.Glas slippers = 250 \n 3. Magic Wand = 500 \n 4. Crown = 750 \n 5. Castles = 1000");
     }
 
-    function transferTokens(address to, uint256 amount) public {
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-        _transfer(msg.sender, to, amount);
+
+    function redeem(uint nextItemId) external
+    {
+        uint256 redeemAmount;
+        require(nextItemId >= 1 && nextItemId <= 5, "Please select an ItemID from the Game item menu");
+
+        if (nextItemId == 1){ redeemAmount = 50; }
+
+        else if (nextItemId == 2) { redeemAmount = 250; }
+
+        else if(nextItemId == 3) { redeemAmount = 500; }
+
+        else if(nextItemId == 4) { redeemAmount = 750; }
+
+        else if(nextItemId == 5) { redeemAmount = 1000; }
+
+        require(balanceOf(msg.sender) >= redeemAmount, "Insufficient tokkens to redeem this item, You can buy more token and purchase this item");
+        _burn(msg.sender, redeemAmount);
+        }
+
+
+    function transferTokens(address to, uint256 trf_amt) public {
+        require(balanceOf(msg.sender) >= trf_amt, "Insufficient token balance");
+        _transfer(msg.sender, to, trf_amt);
     }
 
-    function checkBalance(address account) public view returns (uint256) {
-        return balanceOf(account);
+    function checkBalance(address acc) public view returns (uint256) {
+        return balanceOf(acc);
     }
 
-    function burnTokens(uint256 amount) public {
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-        _burn(msg.sender, amount);
+    function burnTokens(uint256 burn_amt) public {
+        require(balanceOf(msg.sender) >= burn_amt, "Insufficient token balance");
+        _burn(msg.sender, burn_amt);
     }
 }
